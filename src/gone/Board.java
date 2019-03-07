673fc0a45f2c62c1badcf6314dc97aa83be6d760
jyz6 <35541583+jyz6@users.noreply.github.com>
2019-03-07 16:03:34 -0500

    added board file part 2

--- src/gone/Board.java
+++ src/gone/Board.java
@@ -1,38 +1,50 @@
 package gone;
 
+import java.util.*;
+
 public class Board {
-	//TODO: write remaining pseudo code
 	
 	/*
-	 * The Board class stores the board configuration and then runs an algorithm that determines the number of turns it takes to replace every replaceable black piece with a white piece
-	 * and if there are any black pieces left on the board by the end
-	 */
-	
-	/*
-	* hashmap<int, Player> boardConfiguration
-	* The hashmap is a field that stores black and white pieces with a key denoting the location of the piece on the board
-	* The value of the key denotes the space number starting from the top left 
+	 * field representing the board 
 	*/
+	Map<Integer, Piece> boardConfiguration = new HashMap<Integer, Piece>();
+	
 	
 	/*
 	 * Constructor takes a hashmap representation of the board
-	 * private Board(hashmap<int, Player>) {
+	 * 
 	 */
+	private Board(HashMap<Integer, Piece> boardConfiguration) {
+		this.boardConfiguration = boardConfiguration;
+	}
 	
 	/**
 	 * This routine represents the algorithm used to find how many turns it takes to apply the replacement rules until no more replacements can occur
-	 * Rules are as follows:
-	 * 		- Every black piece immediately adjacent to a white piece is replaced by a white piece (no diagonals)
-	 * 		- Repeat until no more replacements can occur
-	 * int findMaxTurns(List<Player_White> initialWhitePieces) {
-	 * 		for each initial white piece
-	 * 			run a depth first search through its list of adjacent pieces
-	 * 			if there are any black pieces in its array of adjacent pieces:
-	 * 				- replace the black piece(s) with a white piece
-	 * 				- increase the number of turns taken by 1 for the root white piece 
-	 * 		
-	 *		return the highest number of turns it took to replace black pieces between all the white pieces
 	 */
+	
+	public int findIterations() {
+		List<Piece> initialWhitePieces = this.findInitialWhitePieces();
+		Piece tempPiece = new Piece();						//placeholder piece used during iteration of list of initial white pieces
+		int totalIterations = 0;							//highest number of iterations
+		int pieceIterations = 0;							//# of iterations for the current white piece
+		Iterator<Piece> iter = initialWhitePieces.iterator();	
+		while(iter.hasNext()) {
+			tempPiece = iter.next();
+			pieceIterations = flipBlackAdjacentsToWhite(tempPiece);
+			if(pieceIterations > totalIterations)
+				totalIterations = pieceIterations;
+		}
+		return totalIterations;
+	}
+	
+	/*
+	 * helper routine to run DFS search through a white_piece to turn all adjacent pieces from black to white
+	 * returns the number of iterations through list of adjacent pieces it took to flip all connect black pieces to white
+	 */
+	public int flipBlackAdjacentsToWhite(Piece whitePiece) {
+		
+	}
+	
 	
 	/*
 	 * List<Player_White> findInitialWhitePieces() {
@@ -40,6 +52,16 @@ public class Board {
 	 * 		If it is a white piece, store its key in a list 
 	 * 		Return the list that represents all the initial white pieces
 	 */
+	
+	public List<Piece> findInitialWhitePieces() {
+		Map<Integer, Piece> board = this.boardConfiguration;
+		List<Piece> initialWhitePieces = new ArrayList<Piece>();
+		for(int key : board.keySet()) {
+			if(board.get(key).isWhite())
+				initialWhitePieces.add(board.get(key));
+		}
+		return initialWhitePieces;
+	}
 	
 	/*
 	 * boolean containsBlackPiece() {
--- src/gone/Piece.java
+++ src/gone/Piece.java
@@ -1,11 +1,60 @@
 package gone;
 
-public interface Piece {
+import java.util.Map;
+
+public class Piece {
 	//TODO Write remaining pseudo-code
+	//int[] adjacent spaces
+	int[] adjacentSpaces;
+	boolean isWhite;
+
 	/*
-	 * Both players should be able to :
-	 * 
-	 * Check adjacency to other pieces
-	 * Check if piece is white
+	 * private Piece(){
+	 * 		adjacent spaces= blank integer array
+	 * }
 	 */
+	public Piece() {
+		adjacentSpaces = new int[4];
+	}
+
+
+	// private Player_Black(board space, number of columns){
+	public Piece(int space, int columns) {
+		//	adjacent spaces = get adjacent pieces(board space, number of columns)
+		adjacentSpaces = adjacentSet(space, columns);
+	}
+
+	/*
+	 * pieces should be able to:
+	 * 
+	 * Check adjacency to other objects
+	 */
+	//(Integer Array) getAdjacentSpaces(board space, number of columns) {
+	int[] adjacentSet(int space, int columns) {
+		//output = array to hold board spaces
+		int[] output = new int[4];
+		//if there is a piece at (This pieces space minus number of columns), and that space to array
+		if((space-columns) > 0) {
+				output[0] = space-columns;
+			}	
+		//if there is a piece at (This pieces space plus number of columns), add that space to array
+			output[1] = space+columns;
+		//if there is a piece at (This space +1) add space to array
+			output[2] = space+1;
+		
+		//if there is a piece at (this space -1) add to array
+			output[3] = space-1;
+		return output;
+	}
+
+
+	// Check if piece is white
+	boolean isWhite(){
+		//piece is not white, return false
+		return isWhite;
+	}
+
+
+
+
 }
